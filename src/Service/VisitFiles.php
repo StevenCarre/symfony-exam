68bf464649df42903d3e3552134123d987594229
StevenCarre <steven.carre@icloud.com>
2022-10-16 22:33:08 +0200

    Exercise 2

--- src/Service/VisitFiles.php
+++ src/Service/VisitFiles.php
@@ -28,22 +28,55 @@ class VisitFiles
     /**
      * Traverse Files & Directories.
      *
-     * Return a list of every files filtered by given function.
+     * Return a list of files filtered by given function.
      *
-     * @param TODO $root
-     * @param TODO $filterFn
+     * @param Directory $root
+     * @param ?callable $filterFn
      *
-     * @return TODO
+     * @return array
      */
-    public function visitFiles($root, callable $filterFn): void
+    public function visitFiles(Directory $root, ?callable $filterFn = null): array
     {
-        // @TODO
+        $result = array();
+
+        foreach ($root->children as $child) {
+            if ($filterFn($child)) {
+                $result[] = match (get_class($child)) {
+                    File::class => $child->name,
+                    Directory::class => [
+                        $child->name => $this->visitFiles($child, $filterFn)
+                    ]
+                };
+            }
+        }
+
+        return $result;
     }
 
-    public function usageExemple(): void
+    /**
+     * @return array
+     * returns [
+     *          [
+     *              "abba" => []
+     *          ],
+     *          "radar"
+     *       ]
+     */
+    public function usageExemple(): array
     {
-        $this->visitFiles(
-            null, // @TODO use a concrete root exemple
+        // example: a directory containing one empty directory and two files, one with a palindromic name
+        $childDir = new Directory('abba', []);
+        $childFile = new File('radar');
+        $childFile2 = new File('test');
+        $root = new Directory('root', [
+            $childDir,
+            $childFile,
+            $childFile2
+        ]);
+
+        return $this->visitFiles(
+            $root,
+            // the callback checks if name of element is a palindrome
             function ($file) {
                 $name = $file->name;
                 for ($i = 0; $i < floor(strlen($name)); $i++) {
